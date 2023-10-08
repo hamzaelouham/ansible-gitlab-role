a4a2860d6915c1aab7d89cd585c60cc77eb8b9a9
hamzaeelouham <hamzaelouham@gmail.com>
2023-10-09 00:02:50 +0100

    fin

---
+++ scripts/credentials.sh
@@ -0,0 +1,9 @@
+subscription="e480db56-5fcc-4df9-9326-42775664845d"
+
+
+az ad sp create-for-rbac --name packer-server --role Contributor --scopes /subscriptions/$subscription
+
+
+
+
+//   /subscriptions/e480db56-5fcc-4df9-9326-42775664845d/resourceGroups/devops-groupe
